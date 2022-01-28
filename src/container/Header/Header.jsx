2f9d50eefbea501e259eac7eccd2a5da41716e4c
Mehdi <mehdi.chamiani@gmail.com>
2022-01-28 18:30:38 +0400

    Navigation added

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import images from '../../constants'
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+
+    </div>
+    <div className="app__wrapper_img">
+      
+    </div>
+  </div>
+);
+
+export default Header;
