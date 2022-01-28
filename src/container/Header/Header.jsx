2bca1b02df784ed440a5074c1896b2db80e69f2b
Mehdi <mehdi.chamiani@gmail.com>
2022-01-28 18:48:44 +0400

    Header added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
-import images from '../../constants'
+import {images }from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div className="app__header app__wrapper section__padding" id="home">
-    <div className="app__wrapper_info">
+	<div className='app__header app__wrapper section__padding' id='home'>
+		<div className='app__wrapper_info'>
+			<SubHeading title='Chase the new flavor' />
 
+			<h1 className='app__header-h1'>The key to Fine Dining</h1>
+			<p className='p__opensans' style={{ margin: '2rem 0' }}>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+				dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<button type='button' className='custom__button'>
+				Explore Menu
+			</button>
+		</div>
+
+		<div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header img" />
     </div>
-    <div className="app__wrapper_img">
-      
-    </div>
-  </div>
+	</div>
 );
 
 export default Header;
