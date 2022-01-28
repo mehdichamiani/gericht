176ba8d5af3fc744e92e6d7f77e34baba8256638
Mehdi <mehdi.chamiani@gmail.com>
2022-01-28 20:28:12 +0400

    Chef section added

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+	<div className='app__bg app__wrapper section__padding'>
+		<div className='app__wrapper_img app__wrapper_img-reverse'>
+			<img src={images.chef} alt='chef' />
+		</div>
+
+		<div className='app__wrapper_info'>
+			<SubHeading title="Chef's Word" />
+
+			<h1 className='headtext__cormorant'>What We Are Up To</h1>
+
+			<div className='app__chef-content'>
+				<div className='app__chef-content_quote'>
+					<img src={images.quote} alt='quote' />
+					<p className='p__opensans'>
+						Magna fermentum iaculis eu non diam phasellus vestibulum eu non diam
+						.
+					</p>
+				</div>
+
+				<p className='p__opensans'>
+					Fringilla ut morbi tincidunt augue interdum velit euismod. Pharetra
+					pharetra massa massa ultricies mi quis hendrerit dolor magna. Sit amet
+					volutpat consequat mauris nunc congue
+				</p>
+			</div>
+
+			<div className='app__chef-sign'>
+				<p>Kevin Lou</p>
+				<p className='p__opensans'>Chef & Founder</p>
+				<img src={images.sign} alt='sign' />
+			</div>
+		</div>
+	</div>
 );
 
 export default Chef;
