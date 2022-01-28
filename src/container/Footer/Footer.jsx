cc58354d1899fb9a35d026c9c558ac15ea4acf4b
Mehdi <mehdi.chamiani@gmail.com>
2022-01-29 00:46:48 +0400

    Footer section finished

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -2,12 +2,58 @@ import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
 import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
 	<div className='app__footer section__padding'>
 		<FooterOverlay />
 		<Newsletter />
+
+		<div className='app__footer-links'>
+			<div className='app__footer-links_contact'>
+				<h1 className='app__footer-headtext'>Contact Us</h1>
+				<p className='p__opensans'>
+					9842 Phoenix Dr Mohave Valley, Arizona(AZ), 86440
+				</p>
+				<p className='p__opensans'>+1 212-345-6789</p>
+				<p className='p__opensans'>+1 212-548-6789</p>
+			</div>
+
+			<div className='app__footer-links_logo'>
+				<img src={images.gericht} alt='footer logo' />
+				<p className='p__opensans'>
+					The best way to find yourself is to lose yourself in the service of
+					others
+				</p>
+				<img
+					src={images.spoon}
+					alt='spoon'
+					className='spoon__img'
+					style={{ marginTop: 15 }}
+				/>
+
+				<div className='app__footer-links_icons'>
+					<FiFacebook />
+					<FiTwitter />
+					<FiInstagram />
+				</div>
+			</div>
+
+			<div className='app__footer-links_work'>
+				<h1 className='app__footer-headtext'>Working Hours</h1>
+				<p className='p__opensans'>Monday-Friday:</p>
+				<p className='p__opensans'>10:00 Am - 02:00 Am</p>
+				<p className='p__opensans'>Saturday-Sunday:</p>
+				<p className='p__opensans'>10:00 Am - 03:00 Am</p>
+			</div>
+		</div>
+
+		<div className='footer__copyright'>
+			<p className='p__opensans'>
+				Powered by React - Made with 💗 by Mehdi Chamiani @ 2022
+			</p>
+		</div>
 	</div>
 );
 
